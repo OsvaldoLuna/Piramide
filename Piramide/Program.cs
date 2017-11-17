baff21c5718bffef8063737fa2c8214b5c38be21
Osvaldo Luna <osvaldounam18@gmail.com>
2017-11-16 19:57:17 -0600

    Piramide de Pascal, Solo muestra

---
+++ Piramide/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piramide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("              1 ");
+            Console.WriteLine("             1 1");
+            Console.WriteLine("            1 2 1");
+            Console.WriteLine("           1 3 3 1");
+            Console.WriteLine("          1 4 6 4 1");
+            Console.WriteLine("         1  5 10 5 1");
+            Console.WriteLine("       1 6 15 20 15 6 1");
+            Console.WriteLine("     1 7 21 35 35 21 7 1");
+            Console.WriteLine("   1 8 28 56 70 56 28 8 1");
+            Console.WriteLine(" 1 9 36 84 126 126 84 36 9 1");
+            Console.ReadKey();
+        }
+    }
+}
